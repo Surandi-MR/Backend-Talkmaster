5a7eaffe033861220d9c35b088646117925e8fa7
Shehani Dissanayake <dissanayakeshehani798@gmail.com>
2025-01-19 04:00:27 +0530

    Delete aplication propeties

--- src/main/java/com/talkmaster/talkmaster/repository/PackageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.talkmaster.talkmaster.repository;
-
-import com.talkmaster.talkmaster.model.PackageModel;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PackageRepository extends MongoRepository<PackageModel, String> {
-}
--- src/src/main/java/com/talkmaster/talkmaster/enums/UserRole.java
+++
@@ -1,7 +0,0 @@
-package com.talkmaster.talkmaster.enums;
-
-public enum UserRole {
-    STUDENT,
-    INSTRUCTOR,
-    ADMIN
-}
